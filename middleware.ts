d43bd9070fd41cba524188e339caa707fcb0dfbf
lukacera <lukaceranic38@gmail.com>
2024-05-26 13:28:00 +0200

    Fix middleware; sending variables from middleware to next function handler

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,47 @@
-import jwt from 'jsonwebtoken';
-
 import { NextRequest, NextResponse } from "next/server";
+import {jwtVerify} from "jose"
 
-
-interface customRequest extends NextRequest {
-    decodedId: string
-}
-
-// Protects route, so only signed User can access it
-export async function middleware(req: customRequest) {
+export async function middleware(req: NextRequest) {
+  try {
     const { pathname } = req.nextUrl;
 
     // Exclude /api/auth/* routes
     if (pathname.startsWith('/api/auth')) {
-        return NextResponse.next();
+      return NextResponse.next();
     }
-    console.log("Run middleware")
 
-    /*
-        Extract token from Authorization header, if it exists
-        Authorization: Bearer <JWT>
-    */
-    const token = req.headers.get("Authorization")?.startsWith("Bearer ") ? req.headers.get("Authorization")?.split(" ")[1] : undefined;
+    // Extract token from Authorization header, if it exists
+    const authHeader = req.headers.get("Authorization");
+    const token = authHeader && authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
+    
     if (!token) {
-        // No token provided
-        return NextResponse.json({message: "Token not found!"}, {status: 401});
+      return NextResponse.json({ message: "Token not found!" }, { status: 401 });
     }
 
-    try {
-        // Verify token and put it on request
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET || "");
-        
-        req.decodedId = decoded.id
+    const { payload }:any = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET || ""));
 
-        return NextResponse.next()
-    } catch (error) {
-        return NextResponse.json({message: "Not authorized!"}, {status: 401});
+    if (!payload) {
+      return NextResponse.json({ message: "Invalid token payload!" }, { status: 401 });
     }
-};
+
+    const requestHeaders = new Headers(req.headers)
+    requestHeaders.set('id', payload.id)
+
+    // You can also set request headers in NextResponse.rewrite
+    return NextResponse.next({
+        request: {
+            headers: requestHeaders
+        }
+    })
+  } catch (error: any) {
+    console.error("Error in middleware:", error);
+    return NextResponse.json(
+        { message: "Not authorized!", error: error.message }, 
+        { status: 401 }
+    );
+  }
+}
 
 export const config = {
-    matcher: '/api/:path*',
-}
+  matcher: '/api/:path*',
+};
