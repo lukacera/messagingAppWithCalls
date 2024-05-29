f7d7005eb07dff450634908ec5f2219052eb763e
lukacera <lukaceranic38@gmail.com>
2024-05-29 16:35:59 +0200

    Make context hook that will be responsible for fetching and handling user data

--- app/utils/fetchFunctions/getCurrentUserDataAPI.ts
+++ app/utils/fetchFunctions/getCurrentUserDataAPI.ts
@@ -3,18 +3,19 @@ export const getCurrentUserData = async () => {
         const response = await fetch("/api/users/myProfile", {
             method: "GET",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
             }        
         });
 
         const data: any = await response.json();
 
+        console.log(data)
         if (!response.ok) {
             return data.error ?? 'An unknown error occurred';
         }
-
-       return data.data // Returns data about currentUser
-        
+        return data.data // Returns data about currentUser
+            
     } catch (error) {
         console.error('Error during login:', error);
         throw new Error('Operation failed!');
---
+++ contexts/UserContext.tsx
@@ -0,0 +1,42 @@
+"use client"
+import React, { createContext, useState, ReactNode, Dispatch, SetStateAction, useEffect } from 'react';
+import { UserType } from "@/app/types/userType";
+import { getCurrentUserData } from '@/app/utils/fetchFunctions/getCurrentUserDataAPI';
+
+type OmittedUser = Omit<UserType, 'password_hash' | 'created_at' | 'updated_at'>
+
+interface UserContextType {
+    currentUserData: OmittedUser;
+    setCurrentUserData: Dispatch<SetStateAction<OmittedUser>>;
+}
+
+// Context that will be used in components
+export const UserContext = createContext<UserContextType>({} as UserContextType);
+
+// UserProvider, for wrapping components that use UserContext
+export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+
+    const [currentUserData, setCurrentUserData] = useState<OmittedUser>({
+        username: '',
+        contacts: [],
+    });
+
+    // Fetch user's data whenever new user logs in
+    useEffect(() => {
+        const fetchCurrentUser = async () => {
+            const user = await getCurrentUserData();
+            setCurrentUserData(user);
+        };
+        fetchCurrentUser();
+        console.log("Fetched!")
+        console.log(currentUserData)
+    }, [currentUserData._id]);
+
+    return (
+        <UserContext.Provider value={{
+            currentUserData, 
+            setCurrentUserData}}>
+            {children}
+        </UserContext.Provider>
+    );
+};
