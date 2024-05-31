b641e1a914bd0be7aea10dd5bee8e3109095883c
lukacera <lukaceranic38@gmail.com>
2024-05-31 18:11:19 +0200

    Update

--- app/api/users/myProfile/route.ts
+++ app/api/users/myProfile/route.ts
@@ -1,19 +1,21 @@
-import { connectToDB } from "@/app/configs/db";
 import User from "@/app/models/User";
+import Conversation from "@/app/models/Conversation";
 import { NextRequest, NextResponse } from "next/server";
 
 // This route returns data about user that is currently logged in
 export const GET = async (req: NextRequest) => {
     try {
         const currentUserID = req.headers.get("id")
-        const currentUser = await User.findById(currentUserID)
-        .select("-password_hash -created_at -updated_at") // Exclude those fields from data that is returned to FE
-        .populate("contacts") 
         
+        const currentUser = await User.findById(currentUserID)
+        .select("-password_hash -created_at -updated_at")
+        .populate("conversations")
+
         return NextResponse.json({
             data: currentUser
         }, {status: 200}) // Success, 200 status
     } catch (error) {
+        console.error(error); // Log the error to the console
         return NextResponse.json(
             {message: "Error occured while fetching current user's data!"}, 
             {status: 500} // Error occured, 500 status
--- app/models/Conversation.ts
+++ app/models/Conversation.ts
@@ -1,14 +1,16 @@
 import { model, Schema, Model, models } from 'mongoose';
-import {ConversationType} from "@/app/types/conversationType"
+import { ConversationType } from "@/app/types/conversationType";
 
 // Schema for conversation; it accepts both group chats and chats between 2 people
 const ConversationSchema: Schema<ConversationType> = new Schema({
-    participants: [{type: Schema.ObjectId, ref:"User",  minlength: 2, required: true}], // Minimum of 2 people can be in chat
+    participants: [
+        { type: Schema.ObjectId, ref: "User", required: true }
+    ],
     messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
 
-const Conversation: Model<ConversationType> = models.Conversation || model<ConversationType>("Conversation", ConversationSchema );
+const Conversation: Model<ConversationType> = models.Conversation || model<ConversationType>("Conversation", ConversationSchema);
 
-export default Conversation
+export default Conversation;
